498bc6ad49cef9872416e56903eddb13f484cc42
Mathius Johnson <mathius.johnson@shopify.com>
2025-01-08 22:34:34 -0800

    feat: add subtract

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #[derive(Debug)]
 enum Expr {
     Number(i64),
-    Add(Box<Expr>, Box<Expr>)
+    Add(Box<Expr>, Box<Expr>),
+    Subtract(Box<Expr>, Box<Expr>)
 }
 
 
@@ -14,6 +15,7 @@ fn eval(expr: &Expr) -> Result<i64, EvalError> {
     match expr {
         Expr::Number(x) => Ok(*x),
         Expr::Add(left, right) => Ok(eval(left)? + eval(right)?),
+        Expr::Subtract(left, right) => Ok(eval(left)? - eval(right)?),
     }
 }
 
@@ -22,17 +24,63 @@ enum ParseError {
     NotANumber
 }
 
+fn popStack(mut stack: Vec<Expr>, callback: Expr) {
+    match (stack.pop(), stack.pop()) {
+        (Some(left), Some(right)) => {
+            // Find a way to make Expr:Add dynamic so we can pass add, subtract whatever into the stack on this function call?
+            stack.push(Expr::Add(Box::new(left), Box::new(right)));
+        }
+        (_, right) =>{
+            println!("only right value in the stack!")
+        }
+        (left, _) =>{
+            println!("only left value in the stack!")
+        }
+        (_, _) => {
+            println!("nothing in the stack!")
+        }
+    }
+}
+
 fn parse(input: &str) -> Result<Expr, ParseError> {
     let mut stack: Vec<Expr> = Vec::new();
     for word in input.split_ascii_whitespace() {
         match word {
             "+" => {
-                if let Some(left) = stack.pop() {
-                    if let Some(right) = stack.pop() {
-                        stack.push(Expr::Add(Box::new(left), Box::new(right)))
+                match (stack.pop(), stack.pop()) {
+                    (Some(left), Some(right)) => {
+                        stack.push(Expr::Add(Box::new(left), Box::new(right)));
+                    }
+                    (_, right) =>{
+                        // Add ParseError's for each of these exceptions
+                        println!("only right value in the stack!")
+                    }
+                    (left, _) =>{
+                        println!("only left value in the stack!")
+                    }
+                    (_, _) => {
+                        println!("nothing in the stack!")
                     }
                 }
                 // Add error handling?
+            }
+            "-" => {
+                match (stack.pop(), stack.pop()) {
+                    (Some(left), Some(right)) => {
+                        stack.push(Expr::Subtract(Box::new(left), Box::new(right)));
+                    }
+                    (_, right) =>{
+                        // Add ParseError's for each of these exceptions
+                        println!("only right value in the stack!")
+                    }
+                    (left, _) =>{
+                        println!("only left value in the stack!")
+                    }
+                    (_, _) => {
+                        println!("nothing in the stack!")
+                    }
+                }
+
             }
             _ => {
                 let x = word.parse::<i64>().map_err(|_| ParseError::NotANumber)?;
@@ -65,6 +113,14 @@ mod tests {
         let expr = parse(input).unwrap();
         let value = eval(&expr).unwrap();
         assert_eq!(value, 8);
+    }
+
+    #[test]
+    fn subtract_test() {
+        let input = "5 3 -";
+        let expr = parse(input).unwrap();
+        let value = eval(&expr).unwrap();
+        assert_eq!(value, -2);
     }
 
     #[ignore]
